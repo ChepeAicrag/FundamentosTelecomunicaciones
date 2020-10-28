99c895a122aa74fb36d280162031116afa2e37ec
ChepeAicrag <chepeaicrag12@gmail.com>
2020-10-28 02:43:13 -0600

    Agregando la GUI.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FundamentosTelecomunicaciones
 Programas de la asignatura de Fundamentos de Telecomunicaciones. 
+
+### 
+Dado un tren de n bits generar un polinomio de grado n - 1. 
+Realizar de forma inversa, dado un polinomio, generar el tren de bits.
+###   
--- Unidad2/src/ProyectoUnidad2.java
+++ Unidad2/src/ProyectoUnidad2.java
@@ -1,26 +1,90 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class ProyectoUnidad2 {
 
     public static void main(String[] args) {
         ProyectoUnidad2 p = new ProyectoUnidad2();
-        p.init();
+        Ventana v = new Ventana();
+    }
+}
+
+class Ventana extends JFrame{
+
+    private JLabel etqEntrada, resultado;
+    private JTextArea txtEntrada;
+    private JButton btnCalcular, btnBorrar;
+
+    public  Ventana(){
+        super("Fundamentos de Telecomunicaciones - Proyecto Unidad 2");
+        setVisible(true);
+        setResizable(false);
+        setContentPane(getElements());
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        //revalidate();
     }
 
-    public void init(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce el tren de bits o polinomio");
-        String entrada = sc.next();
-        //if(entrada.isEmpty() || entrada.length() > 8 ) System.out.println("Introduzca correctamente los datos");
-        String polinomio = bits_polinomio(entrada.trim());
-        System.out.println("El polinomio de " + entrada + " es: " + polinomio);
-        System.out.println("El tren de bits de " + polinomio + " es: " + polinomio_bits(polinomio));
+    public JPanel getElements(){
+        SpringLayout s = new SpringLayout();
+        JPanel panel = new JPanel(s);
+        panel.setBackground(Color.GRAY);
+        etqEntrada = new JLabel("Introduce (tren de bits / polinomio)",SwingConstants.CENTER);
+        etqEntrada.setFont(new Font("Sylfaen", Font.BOLD, 17));
+        txtEntrada = new JTextArea();
+
+        panel.add(etqEntrada);
+        s.putConstraint(SpringLayout.WEST, etqEntrada, 70, SpringLayout.WEST, panel);
+        s.putConstraint(SpringLayout.EAST, etqEntrada, -70, SpringLayout.EAST, panel);
+        s.putConstraint(SpringLayout.NORTH, etqEntrada, 100, SpringLayout.NORTH, panel);
+
+        panel.add(txtEntrada);
+        s.putConstraint(SpringLayout.WEST, txtEntrada, 100, SpringLayout.WEST, panel);
+        s.putConstraint(SpringLayout.EAST, txtEntrada, -100, SpringLayout.EAST, panel);
+        s.putConstraint(SpringLayout.NORTH, txtEntrada, 20, SpringLayout.SOUTH, etqEntrada);
+        s.putConstraint(SpringLayout.SOUTH, txtEntrada, -300, SpringLayout.SOUTH, panel);
+
+        btnCalcular = new JButton("Calcular");
+        btnCalcular.setFont(new Font("Sylfaen", Font.BOLD, 17));
+        panel.add(btnCalcular);
+        s.putConstraint(SpringLayout.WEST, btnCalcular, 140, SpringLayout.WEST, panel);
+        //s.putConstraint(SpringLayout.EAST, btnCalcular, -10, SpringLayout.EAST, panel);
+        s.putConstraint(SpringLayout.NORTH, btnCalcular, 40, SpringLayout.SOUTH, txtEntrada);
+        //s.putConstraint(SpringLayout.SOUTH, btnCalcular, -200, SpringLayout.SOUTH, panel);
+
+        btnBorrar = new JButton("Borrar");
+        btnBorrar.setFont(new Font("Sylfaen", Font.BOLD, 17));
+        btnBorrar.setActionCommand("Borrar");
+        panel.add(btnBorrar);
+        s.putConstraint(SpringLayout.WEST, btnBorrar, 20, SpringLayout.EAST, btnCalcular);
+        //s.putConstraint(SpringLayout.EAST, btnCalcular, -10, SpringLayout.EAST, panel);
+        s.putConstraint(SpringLayout.NORTH, btnBorrar, 40, SpringLayout.SOUTH, txtEntrada);
+        //s.putConstraint(SpringLayout.SOUTH, btnCalcular, -200, SpringLayout.SOUTH, panel);
+
+        resultado = new JLabel("-------------------------------");
+        resultado.setBackground(Color.RED);
+        panel.add(resultado);
+        s.putConstraint(SpringLayout.WEST, resultado, 170, SpringLayout.WEST, panel);
+        s.putConstraint(SpringLayout.EAST, resultado, -170, SpringLayout.EAST, panel);
+        s.putConstraint(SpringLayout.NORTH, resultado, 40, SpringLayout.SOUTH, btnCalcular);
+        s.putConstraint(SpringLayout.SOUTH, resultado, -150, SpringLayout.SOUTH, panel);
+
+        Controlador c = new Controlador();
+        txtEntrada.addKeyListener(c);
+        btnCalcular.addActionListener(c);
+        btnBorrar.addActionListener(c);
+        return panel;
     }
 
-    // Entrada tipo x^3+x^2+x+1
+
     public String polinomio_bits(String entrada){
         String bits = "";
-        String data[] = entrada.split("\\+");
+        String[] data = entrada.split("\\+");
         int maxExpo = Integer.parseInt(""+data[0].charAt(data[0].length() - 1)) + 1;
         boolean [] pos = new boolean[maxExpo];
         for(int i = 0; i < maxExpo; i++) {
@@ -37,8 +101,7 @@ public class ProyectoUnidad2 {
         for(int i = pos.length - 1; i >= 0; i--) bits += pos[i] ? "1" : "0";
         return  bits;
     }
-    // Ya validada la entrada 1001 =
-    // No acepta vacios o con 0
+
     public String bits_polinomio(String entrada){
         String polinomio = "";
         int limit = entrada.length() - 1;
@@ -49,4 +112,41 @@ public class ProyectoUnidad2 {
         polinomio += entrada.endsWith("1") ? "1+" : "";
         return  polinomio.substring(0, polinomio.length() - 1);
     }
+
+
+    class Controlador implements ActionListener, KeyListener {
+
+        // Validar el orden de entrada
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getActionCommand().equals("Borrar")){
+                resultado.setText("-------------------------------");
+                txtEntrada.setText("");
+                return;
+            }
+            String entrada = txtEntrada.getText();
+            if ( entrada.isEmpty() ||
+                 (entrada.contains("x") && entrada.contains("0"))
+            ) return;
+            String salida = entrada.contains("0") && entrada.contains("1") ?
+                            bits_polinomio(entrada) :
+                            polinomio_bits(entrada);
+            resultado.setText(salida);
+            resultado.setAlignmentX(SwingConstants.CENTER);
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            char car = e.getKeyChar();
+                if((car<'0' || car>'9') && (car<'x' || car>'x') && (car<'^' || car>'^') && (car<'+' || car>'+'))
+                    e.consume();
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {}
+
+        @Override
+        public void keyReleased(KeyEvent e) {}
+    }
 }
+
