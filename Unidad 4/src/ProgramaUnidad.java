28a799ff01029cbc68678b214601191ee555a3b9
Chepe Aicrag <chepeaicrag12@gmail.com>
2020-12-21 17:36:27 -0600

    TDM Asyncrhonich

--- Unidad 4/src/ProgramaUnidad.java
+++ Unidad 4/src/ProgramaUnidad.java
@@ -4,7 +4,7 @@
  * @version: 19/12/2020
  *
  * */
-import java.lang.reflect.Array;
+
 import java.util.*;
 
 public class ProgramaUnidad {
@@ -20,16 +20,16 @@ public class ProgramaUnidad {
         System.out.print("Introduce el numero de estaciones: ");
         numEstaciones = sc.nextInt(); // Lectura del número de estaciones
         crearEstaciones(); // Creamos las estaciones
-        rellenarEstaciones(); // Rellenamos las estaciones
-        mostrarTrama(); // Obtenemos y mostramos la trama
-        System.out.println("\nINFORMACIÓN: ");
-        System.out.println("\n" + getBitsTransmitidos() + " bps = " + getMaxCaracter()
-                + " tramas/segundo x " + (8 * numEstaciones + 1) + " bits/trama"); // Información de la tasa de datos
-        System.out.println("Número de bits transmitidos: " + getBitsTransmitidos()); // Información de bits transmitidos
-        System.out.println("Número de bits útiles transmitidos: " + getBitsUtiles()); // Información de bits utiles
-    }
 
+        //System.out.println(getNumEstacionesEnvian());
+        System.out.println(getRanurasTiempoAsincrona());
+
+        rellenarEstaciones(); // Rellenamos las estaciones
+        mostrarDatos(); // Muestra los datos que se enviarán
+        mostrarTramas(); // Obtenemos y mostramos la trama
+    }
     /**
+
      * Crear las estaciones con su respectivo número de caracteres a transmitir
      * */
     public static void crearEstaciones(){
@@ -46,13 +46,13 @@ public class ProgramaUnidad {
      * Genera la trama a partir de las estaciones
      * @return String - La trama generada
      * */
-    public static String generarTrama(){
+    public static String generarTramaSincrona(){
         String tramas = "", trama = "";
         int bit_tramado = 1;
         for(int t = getMaxCaracter() - 1; t >= 0; t--){
             trama = "[";
             for(int i = numEstaciones - 1; i >= 0; i--){
-                ArrayList<String> datos = estaciones.get(i).getData(); // Obtenemos los caracteres de la estación
+                ArrayList<Character> datos = estaciones.get(i).getData(); // Obtenemos los caracteres de la estación
                 try {
                     trama += " " + datos.get(t) + " |"; // Lo colocamos en la trama si es que tiene
                 }catch (Exception error){
@@ -66,18 +66,102 @@ public class ProgramaUnidad {
     }
 
     /**
-     * Permite mostrar la trama generado con un separador
+     * Genera la trama del TDM Asincrono
      * */
-    public static void mostrarTrama(){
-        System.out.println("\nTRAMA GENERADA: \n");
-        String trama = generarTrama(); // Generamos la trama
-        for(int i = 0; i < trama.length(); i++)
-            System.out.printf("*");
-        System.out.println("\n\n" + trama + "\n"); // Mostramos la trama
-        for(int i = 0; i < trama.length(); i++)
-            System.out.printf("*");
-        System.out.println();
+    public static String generarTramaAsincrona(){
+        String tramas = "", trama = "";
+        int bit_tramado = 1,
+            numRanuras = getRanurasTiempoAsincrona();
+        for(int t = numEstaciones - 1; t >= 0; t--){
+            trama = "[";
+            for(int i = numRanuras; i >= 0; i--){
+                ArrayList<Character> datos = estaciones.get(i).getData(); // Obtenemos los caracteres de la estación
+                try {
+                    int direccion = estaciones.get(i).getId(); // Para indicar el direccionamiento
+                    trama += " " + datos.get(t) + direccion + " |"; // Lo colocamos en la trama si es que tiene
+                    if(numRanuras < datos.size() && t == numEstaciones - 1)
+                        trama += " " + datos.get(t + 1) + direccion + " |"; // Le coloca su otro dato porque ya es la ultima trama
+                }catch (Exception error){}
+            }
+            trama = trama.substring(0 ,trama.length() - 1); // Quitar el | al final de cada trama
+            tramas += trama + "] [" + (bit_tramado = Math.abs(bit_tramado - 1)) + "] --- "; // Concatenamos a las tramas y su bit de tramado
+        }
+        return tramas;
     }
+
+    /**
+     * Retorna el número de estaciones que envían datos
+     * */
+    public static int getNumEstacionesEnvian(){
+        return (int)(estaciones.stream().filter(estacion -> estacion.getNumCaracter() > 0).count());
+    }
+
+    /**
+     * Calcula las ranuras de tiempo para las tramas del TDM Asincrono
+     * */
+    public static int getRanurasTiempoAsincrona(){
+        int caracteres = estaciones.stream().mapToInt(Estacion::getNumCaracter).sum(),
+            numEstacionesEnvian = getNumEstacionesEnvian(),
+            aprox = caracteres / numEstacionesEnvian;
+        System.out.println(aprox);
+        return  aprox < numEstacionesEnvian ? aprox : aprox - 1;
+    }
+
+    /**
+     * Calcula el número de bits para direccionamiento de acuerdo a las estaciones
+     * */
+    public static int getBitsDireccionamiento(){
+        return toBinary(numEstaciones).length();
+    }
+
+    /**
+     * Convierte un número a binario
+     * */
+    public static String toBinary(int n){
+        if(n == 1) return "1";
+        return toBinary(n / 2) + n % 2;
+    }
+
+
+    /**
+     * Permite mostrar las tramas generadas
+     * */
+    public static void mostrarTramas(){
+        System.out.println("\nTRAMA SINCRONA GENERADA: \n");
+        String trama = generarTramaSincrona(); // Generamos la trama
+        decoracion(trama);
+        System.out.println("\n\n" + trama + "\n"); // Mostramos la trama
+        decoracion(trama);
+        System.out.println("\nINFORMACIÓN  DE TDM SINCRONA: ");
+        System.out.println("\n" + getBitsTransmitidosSincrona() + " bps = " + getMaxCaracter()
+                + " tramas/segundo x " + (8 * numEstaciones + 1) + " bits/trama"); // Información de la tasa de datos
+        System.out.println("Número de bits transmitidos: " + getBitsTransmitidosSincrona()); // Información de bits transmitidos
+        System.out.println("Número de bits útiles transmitidos: " + getBitsUtiles()); // Información de bits utiles
+
+        System.out.println("\n\nTRAMA ASINCRONA GENERADA: \n");
+        String tramaAsincrona = generarTramaAsincrona();
+        decoracion(tramaAsincrona);
+        System.out.println("\n\n" + tramaAsincrona + "\n"); // Mostramos la trama asincrona
+        decoracion(tramaAsincrona);
+        System.out.println("\nINFORMACIÓN DE TDM ASINCRONA: ");
+        System.out.println("Número de bits transmitidos: " + getBitsTransmitidosSincrona()); // Información de bits transmitidos
+        System.out.println("Número de bits útiles transmitidos: " + getBitsUtiles()); // Información de bits utiles
+        System.out.println("Bits para el direccionamiento: " + getBitsDireccionamiento());
+    }
+
+    public static void decoracion(String trama){
+        for(int i = 0; i < trama.length(); i++)
+            System.out.printf("*");
+    }
+
+    /**
+     * Mostrar los datos que envía cada estación
+     * */
+    public static void mostrarDatos(){
+        System.out.println("DATOS DE CADA ESTACIÓN");
+        estaciones.stream().forEach(e -> e.imprimirDatos());
+    }
+
 
     /**
      * Rellena todas las estaciones ejecutando su método para agregar caracteres de cada estación
@@ -89,7 +173,7 @@ public class ProgramaUnidad {
     /**
      * Calcula y devuelve los bits que se han transmitido
      * */
-    public static int getBitsTransmitidos(){
+    public static int getBitsTransmitidosSincrona(){
         return getMaxCaracter() * numEstaciones * 8 + getMaxCaracter();
     }
 
@@ -113,7 +197,7 @@ public class ProgramaUnidad {
  * */
 class Estacion{
 
-    private ArrayList<String> data; // Información que transmite
+    private ArrayList<Character> data; // Información que transmite
     private int bits, // Cantidad de bits que transmite
                 numCaracter, // Número de caracteres de la estacion
                 id; // Identificador
@@ -132,19 +216,20 @@ class Estacion{
      * Permite agregar los caracteres a la estación
      * */
     public void addCaracters(){
-        String [] abecedario = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R",
-                "S", "T", "U", "V", "W", "X", "Y", "Z" }; // Caracteres para cada estación
+        Scanner sc = new Scanner(System.in);
         System.out.println("Caracteres de Estación " + id);
-        String datos = "[" ;
         for(int i = 0; i < numCaracter; i++){
-            String c = id <= abecedario.length ? abecedario[id - 1]
-                    : abecedario[id - abecedario.length - 1] + id / abecedario.length + 1; // Calcula el caracter de acuerdo a la estación
-            datos += " " + c; // Agrega a la cadena para mostrar los datos
-            data.add(c); // Agrega el caracter a los datos de la estación
+            System.out.printf("Caracter [" + (i + 1) + "] = ");
+            data.add(new Character(sc.next().charAt(0))); // Agrega el caracter a los datos de la estación
         }
-        datos += " ]";
-        System.out.println(datos);
         System.out.println("-------------------------------------");
+    }
+
+    /**
+     * Devuelve el número de indetificación de la estacipon
+     * */
+    public int getId(){
+        return id;
     }
 
     /**
@@ -164,7 +249,17 @@ class Estacion{
     /**
      * Devuelve el array de caracteres que envía la estación
      * */
-    public ArrayList<String> getData(){
+    public ArrayList<Character> getData(){
         return data;
+    }
+
+    /**
+     * Imprime los datos que envía y como los envía la estación
+     * */
+    public void imprimirDatos(){
+        System.out.printf("ESTACIÓN " + id + " --> ENVÍA --> [ ");
+        StringBuilder info = new StringBuilder();
+        data.forEach(e -> info.append(e.toString()+ " "));
+        System.out.printf(info + "] --> DE LA FORMA --> [" + info.reverse() + " ]\n");
     }
 }
